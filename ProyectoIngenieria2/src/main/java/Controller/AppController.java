5eef8e8b22763dec1369e2bb0def3fea29933cb4
Isa <Isa@192.168.0.100>
2016-02-24 16:07:43 -0600

    Change the method POst -Get and the form in AgregarContacto

--- ProyectoIngenieria2/src/main/java/Controller/AppController.java
+++ ProyectoIngenieria2/src/main/java/Controller/AppController.java
@@ -144,7 +144,7 @@ public class AppController {
         return "agregarContacto";
     }
     
-    @RequestMapping(value = {"/AgregarContactosKinder"}, method = RequestMethod.POST)
+    @RequestMapping(value = {"/AgregarContactosKinder"}, method = RequestMethod.GET)
     public String addContacto(@Valid Contacto contacto, BindingResult result, ModelMap model) {
         if(result.hasErrors()) {
             return "agregarContacto";
